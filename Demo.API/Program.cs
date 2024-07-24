e739c5982cc1304b6388fb23427b01658c160552
karthik-wonderbiz <karthik.balakrishnan@wonderbiz.in>
2024-07-24 15:04:31 +0530

    cors

--- Demo.API/Program.cs
+++ Demo.API/Program.cs
@@ -23,6 +23,17 @@ builder.Services.AddScoped<IUserServices, UserServices>();
 
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp",
+        builder =>
+        {
+            builder.AllowAnyOrigin() // Angular app URL
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 
@@ -31,6 +42,9 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("AllowAngularApp");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
